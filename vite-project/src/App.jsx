8d84d2452c45355037cc83a90e706030a5f57031
Avishukla-student <avishukla8303@gmail.com>
2025-06-06 21:10:43 +0530

    first project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200 "
+        style={{ backgroundColor: color }}>
+        <div className='fixed  p-2 flex flex-wrap justify-center bottom-10 inset-x-0 '>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-4 py-2 rounded-3xl'>
+            <button className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-full' onClick={() => setColor('red')}>
+              Red
+            </button>
+            <button className='bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-full' onClick={() => setColor('green')}>
+              Green
+            </button>
+            <button className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full' onClick={() => setColor('blue')}>
+              Blue
+            </button>
+            <button className='bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded-full' onClick={() => setColor('yellow')}>Yellow</button>
+            <button className='bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded-full' onClick={() => setColor('purple')}>
+              Purple
+            </button>
+            <button className='bg-pink-500 hover:bg-pink-600 text-white px-4 py-2 rounded-full' onClick={() => setColor('pink')}>
+              Pink
+            </button>
+            <button className='bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-full' onClick={() => setColor('gray')}>
+              Gray
+            </button>
+            <button className='bg-black hover:bg-gray-800 text-white px-4 py-2 rounded-full' onClick={() => setColor('black')}>
+              Black
+            </button>
+            <button className='bg-white hover:bg-gray-200 text-black px-4 py-2 rounded-full' onClick={() => setColor('white')}>
+              White
+            </button>
+
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
